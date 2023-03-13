57eba43c54b2b04ac677e5266a96984e8d438db2
mohamedkaissi <kaissimohamed67@gmail.com>
2023-03-13 22:28:42 +0000

    support for Protected Index Links

--- scraper.py
+++ scraper.py
@@ -2,23 +2,35 @@ import requests
 import base64 
 import json
 import urllib
+import getpass
 
 
 next_page = False
 next_page_token = "" 
- 
+
 
 def decrypt(string): 
      return base64.b64decode(string[::-1][24:-20]).decode('utf-8')  
 
-  
-def func(payload_input, url): 
+
+def check_protected(url):
+    r = requests.get(url)
+    if r.status_code == 401:
+        print("The index link is password protected.")
+        username = input("Enter username: ")
+        password = getpass.getpass(prompt="Enter password: ")
+        return (username, password)
+    else:
+        return None
+
+    
+def func(payload_input, url, auth=None): 
     global next_page 
     global next_page_token
     
-    url = url + "/" if  url[-1] != '/' else url
-         
-    encrypted_response = requests.post(url, data=payload_input)
+    url = url + "/" if url[-1] != '/' else url
+    
+    encrypted_response = requests.post(url, data=payload_input, auth=auth)
    
     try: decrypted_response = json.loads(decrypt(encrypted_response.text))
     except: return "something went wrong. check index link again"
@@ -35,30 +47,40 @@ def func(payload_input, url):
    
     if list(decrypted_response.get("data").keys())[0] == "error": pass
     else :
-      file_length = len(decrypted_response["data"]["files"])
-      for i, _ in enumerate(range(file_length)):
-	        files_type   = decrypted_response["data"]["files"][i]["mimeType"] 
-	        files_name   = decrypted_response["data"]["files"][i]["name"] 
-	     
-	        if files_type == "application/vnd.google-apps.folder": pass
-	        else:
-	            direct_download_link = url + urllib.parse.quote(files_name)
-	            result += f"{direct_download_link}\n"
-      return result
-	
+        file_length = len(decrypted_response["data"]["files"])
+        for i, _ in enumerate(range(file_length)):
+            files_type   = decrypted_response["data"]["files"][i]["mimeType"] 
+            files_name   = decrypted_response["data"]["files"][i]["name"] 
+         
+            if files_type == "application/vnd.google-apps.folder": pass
+            else:
+                direct_download_link = url + urllib.parse.quote(files_name)
+                result += f"{direct_download_link}\n"
+        return result
+        
+
 def main():
     global next_page 
     global next_page_token
     next_page = False
     next_page_token = "" 
     index_link = input("Enter the index link: ")
+    auth = check_protected(index_link)
     print(f"Index Link: {index_link}\n\n")
     x = 0
+    result = ""
     payload = {"page_token":next_page_token, "page_index": x}	
-    print(func(payload, index_link))
+    result += func(payload, index_link, auth=auth) or ""
     while next_page == True:
         payload = {"page_token":next_page_token, "page_index": x}
-        print(func(payload, index_link))
+        result += func(payload, index_link, auth=auth) or ""
         x += 1
 
-main()
+    output_file = input("\nEnter output file name: ") + ".txt"
+    with open(output_file, "w") as f:
+        f.write(result)
+    print(f"\nResults saved to {output_file}")
+        
+
+if __name__ == "__main__":
+    main()
